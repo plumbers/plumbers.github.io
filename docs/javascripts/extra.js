32726b0010b22e0e929ec9dfab107bd1df2cdcb0
plumbers <admin@mail.com>
2023-07-16 09:36:35 +0300

    compressed ? stringify

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,18 @@
+// extra_javascript:
+//   - javascripts/extra.js
+
+// <script src="https://requirejs.org/docs/release/2.3.5/minified/require.js"></script>
+// import lunr from "lunr";
+
+// require(['fs','lunr'], function (fs,lunr) {
+//     //foo is now loaded.
+//     // const fs = require('fs');
+//     // import { fs } from 'fs.js';
+//     // var lunr = require('lunr');
+//
+//     let data = fs.readFileSync( "site/search/search_index.json" , "utf8" );
+//     var idx = lunr.Index.load(JSON.parse(data));
+//
+//     fs.writeFileSync('index.txt', 'Hello World!');
+//     // fs.writeFileSync('index.txt', idx.search('огон~1'));
+// });
